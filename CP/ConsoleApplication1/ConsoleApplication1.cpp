04f2d562002ab1df265b517687525a7cf2fe5bf0
TheTrueRoy <66959950+TheTrueRoy@users.noreply.github.com>
2021-07-20 01:11:41 -0400

    Initial Commit

---
+++ CP/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <string>
+
+using namespace std;
+
+#define pb push_back
+#define loop(n) for(int i = 0; i < n; i++)
+#define ii pair<int,int>
+#define ipii pair<int, pair<int,int>>
+
+
+int main() {
+	string S;
+	int Y, M, D, N;
+	cin >> N;
+	loop(N) {
+		Y = 1, M = 1, D = 1;
+		cin >> S;
+		if (S.at(0) == '-') {
+			Y *= -1;
+			S.erase(0, 1);
+		}
+		Y *= stoi(S.substr(0, S.find("-")));
+		S.erase(0, S.find("-") + 1);
+		M *= stoi(S.substr(0, S.find("-")));
+		S.erase(0, S.find("-") + 1);
+		D *= stoi(S.substr(0, S.find("-")));
+		S.erase(0, S.find("-") + 1);
+		cout << Y << M << D << endl;
+	}
+}
---
+++ Calendar Contest July/1 - Sexagenary Cycle.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <set>
+
+using namespace std;
+
+#define pb push_back
+#define loop(n) for(int i = 0; i < n; i++)
+#define ii pair<int,int>
+#define ipii pair<int, pair<int,int>>
+
+string stems[10] = { "甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸" };
+string branches[12] = { "子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥" };
+
+int main() {
+	int N, M, O, P;
+	cin >> N;
+	loop(N) {
+		cin >> M; M -= 4;
+		if (M < 0) {
+			M %= 60;
+			M += 60;
+		}
+		O = M % 10;
+		P = M % 12;
+		cout << stems[O] << branches[P] << endl;
+	}
+}
