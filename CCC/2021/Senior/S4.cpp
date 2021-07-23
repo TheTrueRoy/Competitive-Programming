3ea01a3b16a4ced8fff357d693280733787652b9
TheTrueRoy <66959950+TheTrueRoy@users.noreply.github.com>
2021-07-22 20:29:04 -0400

    S4, S5

---
+++ CCC/2021/Senior/S4.cpp
@@ -0,0 +1,81 @@
+﻿﻿#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <string>
+#include <stack>
+#include <climits>
+
+/*	Completion date: 2021/07/22	*
+ *	CCC Grader Score: 15/15		*/
+
+using namespace std;
+
+#define loop(n) for(int i = 0; i < n; i++)
+#define dLoop(n,m) for(int i = 0; i < n; i++) { for(int j = 0; j < m; j++)
+#define ii pair<int,int>
+#define iii pair<int, pair<int,int>>
+#define sf second.first
+#define ss second.second 
+#define ll long long
+#define pb push_back 
+#define mp 
+
+vector<int> walkways[201000];
+int walk[201001];
+bool visited[201000];
+int loc[201001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, W, D;
+	cin >> N >> W >> D;
+
+	for (int a = 0; a < N; a++)
+		walk[a] = INT_MAX / 2;
+
+	walk[N] = 0;
+
+	int t, f;
+	loop(W) {
+		cin >> f >> t;
+		walkways[t].pb(f);
+	}
+
+	queue<int> check;
+	visited[N] = 1;
+	check.push(N);
+	while (!check.empty()) {
+		int cur = check.front(); check.pop();
+		for (auto next : walkways[cur]) {
+			if (!visited[next]) {
+				walk[next] = walk[cur] + 1;
+				visited[next] = true;
+				check.push(next);
+			}
+		}
+	}
+
+	multiset<int> distances;
+
+	loop(N) {
+		cin >> loc[i];
+		distances.insert(i + walk[loc[i]]);
+	}
+
+	loop(D) {
+		int a, b;
+		cin >> a >> b; a--; b--;
+		distances.erase(distances.find(a + walk[loc[a]]));
+		distances.erase(distances.find(b + walk[loc[b]]));
+		swap(loc[a], loc[b]);
+		distances.insert(a + walk[loc[a]]);
+		distances.insert(b + walk[loc[b]]);
+		cout << *distances.begin() << endl;
+	}
+};
---
+++ CCC/2021/Senior/S5.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <string>
+#include <stack>
+#include <climits>
+#include <array>
+
+/*	Completion date: 2021/07/22	*
+ *	CCC Grader Score: 15/15		*/
+
+using namespace std;
+
+#define fori(l,n) for(int i = l; i < n; i++)
+#define forj(l,n) for(int j = l; j < n; j++)
+#define ii pair<int,int>
+#define iii pair<int, pair<int,int>>
+#define sf second.first
+#define ss second.second 
+#define ll long long
+#define pb push_back 
+
+int gcd(int a, int b) {
+	if (b == 0)
+		return a;
+	return gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+	return (a / gcd(a, b)) * b;
+}
+
+const int MAXN = 150010, MAXM = 150010, MAXZ = 26;
+int diff[MAXN][MAXZ], d[MAXZ], nums[MAXN], st[MAXN*4];
+array<int, 3> ins[MAXM];
+
+int check(int l, int h, int index = 1, int low = 1, int high = MAXN) {
+	if (h < l)
+		return 0;
+	if (l == low && h == high)
+		return st[index];
+	int mid = (low + high) / 2;
+	return gcd(check(l, min(mid,h), index * 2, low, mid), check(max(l,mid+1), h, index * 2 + 1, mid + 1, high));
+}
+
+void createTree(int index = 1, int low = 1, int high = MAXN) {
+	if (low == high)
+		st[index] = nums[low];
+	else {
+		int mid = (low + high) / 2;
+		createTree(index * 2, low, mid);
+		createTree(index * 2 + 1, mid + 1, high);
+		st[index] = gcd(st[index * 2], st[index * 2 + 1]);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	int start, end, mag;
+	fori(0,M) {
+		cin >> start >> end >> mag;
+		diff[start][mag]++;
+		diff[end + 1][mag]--;
+		ins[i] = {start, end, mag};
+	}
+	fori(1,N+1) {
+		nums[i] = 1;
+		forj(1,17) {
+			d[j] += diff[i][j];
+			if (d[j] > 0) {
+				nums[i] = lcm(nums[i], j);
+			}
+		}
+	}
+
+	createTree();
+
+	fori(0,M) {
+		auto I = ins[i];
+		if (check(I[0], I[1]) != I[2]) {
+			cout << "Impossible" << endl;
+			return 0;
+		}
+	}
+	fori(1,N+1)
+		cout << nums[i] << ' ';
+	cout << endl;
+};
